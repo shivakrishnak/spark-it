27c1c8cb3b28ffc90871cd7967dc5a6144c08ea4
Shiva Krishna <itzshivakrishna@gmail.com>
2022-10-31 19:54:29 +0530

    update

---
+++ src/main/scala/SparkAppTest.scala
@@ -0,0 +1,14 @@
+import org.apache.spark.sql.SparkSession
+
+object SparkAppTest {
+  def main(args: Array[String]): Unit = {
+    println("Hello world!")
+    val spark = SparkSession.builder().master("local").appName("test").getOrCreate()
+
+    println("First SparkContext:")
+    println("APP Name :" + spark.sparkContext.appName);
+    println("Deploy Mode :" + spark.sparkContext.deployMode);
+    println("Master :" + spark.sparkContext.master);
+    println("ApplicationId " +spark.sparkContext.applicationId)
+  }
+}
